7ac4b9f435f3c2489de25d680a9248a4da5b7aa6
ExtemporaneousnessOne <133890411+ExtemporaneousnessOne@users.noreply.github.com>
2024-08-14 15:01:49 -0400

    commenting out the console logs

--- index.js
+++ index.js
@@ -19,40 +19,40 @@ function setClockDigits(){
   //365 days -> 1 year
 
   let monthsRemaining = ((12 * msRemaining) / (1000*60*60*24*365)); //in ms -> month
-  console.log(monthsRemaining.toString() + " - monthsRemaining"); 
+  // console.log(monthsRemaining.toString() + " - monthsRemaining"); 
   let wholeMonths = Math.floor(Math.abs(monthsRemaining)); //non decimal portion of months
   monthsLeftDiv.textContent = wholeMonths.toString().padStart(2, '0');  
-  console.log(wholeMonths.toString() + " - wholeMonths");
+  // console.log(wholeMonths.toString() + " - wholeMonths");
 
   let weeksRemaining = (((monthsRemaining - Math.floor(Math.abs(monthsRemaining))) * 365) / (12 * 7)); //in month -> week
-  console.log(weeksRemaining.toString() + " - weeksRemaining");
+  // console.log(weeksRemaining.toString() + " - weeksRemaining");
   let wholeWeeks = Math.floor(Math.abs(weeksRemaining)); //non decimal portion of weeks
   weeksLeftDiv.textContent = wholeWeeks.toString().padStart(2, '0');
-  console.log(wholeWeeks.toString() + " - wholeWeeks");
+  // console.log(wholeWeeks.toString() + " - wholeWeeks");
 
   let daysRemaining = ((weeksRemaining - Math.floor(Math.abs(weeksRemaining))) * 7); //in week -> days
-  console.log(daysRemaining.toString() + " - daysRemaining");
+  // console.log(daysRemaining.toString() + " - daysRemaining");
   let wholeDays = Math.floor(Math.abs(daysRemaining)); //non decimal portion of days
   daysLeftDiv.textContent = wholeDays.toString().padStart(2, '0');
-  console.log(wholeDays.toString() + " - wholeDays");
+  // console.log(wholeDays.toString() + " - wholeDays");
 
   let hoursRemaining = ((daysRemaining - Math.floor(Math.abs(daysRemaining))) * 24); //in days -> hours 
-  console.log(hoursRemaining.toString() + " - hoursRemaining");
+  // console.log(hoursRemaining.toString() + " - hoursRemaining");
   let wholeHours = Math.floor(Math.abs(hoursRemaining)); //non decimal portion of hours
   hoursLeftDiv.textContent = wholeHours.toString().padStart(2, '0');
-  console.log(wholeHours.toString() + " - wholeHours");
+  // console.log(wholeHours.toString() + " - wholeHours");
 
   let minutesRemaining = ((hoursRemaining - Math.floor(Math.abs(hoursRemaining))) * 60); //hours -> min
-  console.log(minutesRemaining.toString() + " - minutesRemaining");
+  // console.log(minutesRemaining.toString() + " - minutesRemaining");
   let wholeMinutes = Math.floor(Math.abs(minutesRemaining)); //non decimal portion of minutes
   minutesLeftDiv.textContent = wholeMinutes.toString().padStart(2, '0');
-  console.log(wholeMinutes.toString() + " - wholeMinutes");
+  // console.log(wholeMinutes.toString() + " - wholeMinutes");
 
   let secondsRemaining = ((minutesRemaining - Math.floor(Math.abs(minutesRemaining))) * 60); //mins -> seconds
-  console.log(secondsRemaining.toString() + " - secondsRemaining");
+  // console.log(secondsRemaining.toString() + " - secondsRemaining");
   let wholeSeconds = Math.floor(Math.abs(secondsRemaining));//non decimal portion of seconds
   secondsLeftDiv.textContent = wholeSeconds.toString().padStart(2, '0');
-  console.log(wholeSeconds.toString() + " - wholeSeconds");
+  // console.log(wholeSeconds.toString() + " - wholeSeconds");
 }
 
 
